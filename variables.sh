d093e7953b41feff56b1d6003fd245bca489bf63
Sudeep <sudeepreddy340@gmail.com>
2025-10-15 22:42:02 +0530

    new file

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Ramesh
+Person2=Suresh
+
+echo "$Person1: Hi Suresh, How are you"
+echo "$Person2: Hello Ramesh, I am fine. How are you"
+echo "$Person1: what are you learning recently"
+echo "$Person2: I am learning Shell Scripting"
