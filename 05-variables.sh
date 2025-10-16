adf10bb12ed5734a24e78ba9e478069955fd84c8
Sudeep <sudeepreddy340@gmail.com>
2025-10-16 11:49:36 +0530

    new file-5 secret

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Please enter your PIN Number"
-read PIN_NUMBER pin_number # Here PIN_NUMBER is variable
+read -s PIN_NUMBER pin_number # Here PIN_NUMBER is variable
 
 echo "PIN entered is: $PIN_NUMBER"
