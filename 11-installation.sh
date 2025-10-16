53fbb859884ccb7489e430a35a79ee72327fb551
Sudeep <sudeepreddy340@gmail.com>
2025-10-16 17:32:31 +0530

    new file-8

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,7 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "ERROR: Please run this script with root access"
 else
-    dnf install -y
+    dnf install mysql -y
 
     if [ $? -ne 0 ]; then
         echo "Error: Installing MySQL is failed"
