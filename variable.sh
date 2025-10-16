96972d8a2d6426d95a178689c45e6e37e048fd17
Sudeep <sudeepreddy340@gmail.com>
2025-10-16 11:26:45 +0530

    new file1

---
+++ variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bsah
+
+Person1=Trump
+Person2=Putin
+
+echo "${Person1}: Welcome to ALska Putin"
+echo "${Person2}: Thank you Trump"
+echo "${Person1}: So, do we have a deal"
+echo "${Person2}: Yes, if you give Ukraine to us."
